a4e1c1dff43dc5827ba1b223e1a90845c7043a4f
RafaelH31 <rafael.cebrian@sptech.school>
2023-10-10 20:53:28 -0300

    update(melhorando script de instalação)

--- centrixMonitoramento/src/main/kotlin/scriptPadraoPython.kt
+++ centrixMonitoramento/src/main/kotlin/scriptPadraoPython.kt
@@ -1,20 +1,7 @@
 import java.io.File
 
 object scriptPadraoPython {
-   /* fun baixarBibliotecas() {
 
-        val codigoBash = """
-            sudo apt-get install python-pip;
-            pip install psutil;
-            pip install time;
-            pip install mysql-connector-python
-        """.trimIndent()
-
-        val arquivoPyInstall = "centrixInstall.sh"
-        File(arquivoPyInstall).writeText(codigoBash)
-        Thread.sleep(2 * 1000L)
-        Runtime.getRuntime().exec("Bash $nomeArquivoPyDefault")
-    }*/
     fun criarScript(tempo: Int, idMaquinaDado: Int, idEmpresaDado: Int){
 
         val codigoPythonDefault = """
@@ -73,4 +60,64 @@ object scriptPadraoPython {
         Thread.sleep(9 * 1000L)
         Runtime.getRuntime().exec("python $nomeArquivoPyDefault")
     }
+    /*fun criarScript(tempo: Int, idMaquinaDado: Int, idEmpresaDado: Int) {
+
+        val codigoBash = """
+
+            if ! command -v python3 &> /dev/null; then
+                echo "Python não encontrado. Instalando Python..."
+                sudo apt-get update
+                sudo apt-get install python3
+            fi
+
+            python3 -m ensurepip --default-pip
+            python3 -m pip install --upgrade pip
+
+            python3 -m pip install psutil mysql-connector-python
+
+            cat <<EOL > centrixMonitoramento.py
+            import psutil
+            import time
+            from mysql.connector import connect
+
+            cnx = connect(user='aluno', password='sptech', host='localhost', database='centrix')
+
+            while True:
+                CPU = round(psutil.cpu_percent(), 2)
+                RAM = round(psutil.virtual_memory().used / (1024**3), 3)
+                DISK = round(psutil.disk_usage('/').used / (1024**3), 3)
+
+                bd = cnx.cursor()
+
+                # CPU
+                dados_CPU_PC = [CPU, 1, 1, $idMaquinaDado, $idEmpresaDado]
+                add_leitura_CPU = ("INSERT INTO Monitoramento (Data_captura, Hora_captura, Dado_Capturado, fkCompMonitorados, fkCompMoniExistentes, fkMaqCompMoni, fkEmpMaqCompMoni) VALUES (CURRENT_DATE, CURRENT_TIME, %s, %s, %s, %s, %s)")
+                bd.execute(add_leitura_CPU, dados_CPU_PC)
+
+                # DISK
+                dados_DISK_PC = [DISK, 3, 2, $idMaquinaDado, $idEmpresaDado]
+                add_leitura_DISK = ("INSERT INTO Monitoramento (Data_captura, Hora_captura, Dado_Capturado, fkCompMonitorados, fkCompMoniExistentes, fkMaqCompMoni, fkEmpMaqCompMoni) VALUES (CURRENT_DATE, CURRENT_TIME, %s, %s, %s, %s, %s)")
+                bd.execute(add_leitura_DISK, dados_DISK_PC)
+
+                # RAM
+                dados_RAM_PC = [RAM, 2, 3, $idMaquinaDado, $idEmpresaDado]
+                add_leitura_RAM = ("INSERT INTO Monitoramento (Data_captura, Hora_captura, Dado_Capturado, fkCompMonitorados, fkCompMoniExistentes, fkMaqCompMoni, fkEmpMaqCompMoni) VALUES (CURRENT_DATE, CURRENT_TIME, %s, %s, %s, %s, %s)")
+                bd.execute(add_leitura_RAM, dados_RAM_PC)
+
+                cnx.commit()
+                time.sleep($tempo)
+            EOL
+
+            python3 centrixMonitoramento.py
+        """.trimIndent()
+
+
+        val arquivoInstallScript = "centrixInstall.sh"
+
+        File(arquivoInstallScript).writeText(codigoBash)
+
+        Runtime.getRuntime().exec("chmod +x $arquivoInstallScript")
+
+        Runtime.getRuntime().exec("./$arquivoInstallScript")
+    }*/
 }
