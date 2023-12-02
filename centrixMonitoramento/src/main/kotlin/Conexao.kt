b8ed91f63bb79d01adc1c03721017e7986477596
AlexandreDSptech <alexandre.murata@sptech.school>
2023-12-02 18:34:25 -0300

    update(ajustando coisas do grupo)

--- centrixMonitoramento/src/main/kotlin/Conexao.kt
+++ centrixMonitoramento/src/main/kotlin/Conexao.kt
@@ -4,8 +4,8 @@ import org.springframework.jdbc.core.JdbcTemplate
 object Conexao {
 
     //substitua aqui
-    var bancoUser = "root"
-    var bancoSenha = "38762"
+    var bancoUser = "aluno"
+    var bancoSenha = "sptech"
 
     var bancoUserServer = "sa"
     var bancoSenhaServer = "centrix"
