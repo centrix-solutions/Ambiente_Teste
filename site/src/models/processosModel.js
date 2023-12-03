7511a455bf6acafdcb78e7947ebc3efd239cda24
AlexandreDSptech <alexandre.murata@sptech.school>
2023-12-03 15:34:00 -0300

    update(ajustando coisas do individual)

--- site/src/models/processosModel.js
+++ site/src/models/processosModel.js
@@ -1,9 +1,86 @@
 var database = require("../database/config");
 
-function listarProcessos(idEmpresa, idAndar) {
-    var instrucao = `SELECT * FROM Processo JOIN Empresa ON Processo.idAndar_de_trabalho = Empresa.idEmpresa JOIN andar_de_trabalho ON andar_de_trabalho.fkEmpAndar = Empresa.idEmpresa WHERE Empresa.idEmpresa = ${idEmpresa} AND idAndar_de_trabalho = ${idAndar};`;
-    
-    console.log("Executando a instrução SQL: \n" + instrucao);
+function listarProcessos(idEmpresa, idAndar, filtro) {
+    if (!filtro) {
+        var instrucao = `
+        SELECT
+            Processo.titulo,
+            Maquinas.Id_do_dispositivo
+        FROM
+            Processo
+        JOIN
+            Empresa
+        ON
+            Processo.fkEmpProc = Empresa.idEmpresa
+        LEFT JOIN
+            andar_de_trabalho
+        ON
+            andar_de_trabalho.fkEmpAndar = Empresa.idEmpresa
+        JOIN
+            Maquinas
+        ON
+            Maquinas.fkEmpMaq = Empresa.idEmpresa
+        LEFT JOIN
+            Login
+        ON
+            Login.Id_do_dispositivo = Maquinas.Id_do_dispositivo
+        WHERE
+            Empresa.idEmpresa = 1;`;
+        console.log("Executando listarProcessos sem filtro: \n" + instrucao);
+        return database.executar(instrucao);
+    }
+    if (idAndar == null) {
+        var instrucao = `
+        SELECT
+            Processo.titulo,
+            Maquinas.Id_do_dispositivo
+        FROM
+            Processo
+        JOIN
+            Empresa
+        ON
+            Processo.fkEmpProc = Empresa.idEmpresa
+        LEFT JOIN
+            andar_de_trabalho
+        ON
+            andar_de_trabalho.fkEmpAndar = Empresa.idEmpresa
+        JOIN
+            Maquinas
+        ON
+            Maquinas.fkEmpMaq = Empresa.idEmpresa
+        LEFT JOIN
+            Login
+        ON
+            Login.Id_do_dispositivo = Maquinas.Id_do_dispositivo
+        WHERE
+            Empresa.idEmpresa = ${idEmpresa} AND Maquinas.fkAndarDeTrabalho IS NULL;`;
+    } else {
+        var instrucao = `
+        SELECT
+            Processo.titulo,
+            Maquinas.Id_do_dispositivo
+        FROM
+            Processo
+        JOIN
+            Empresa
+        ON
+            Processo.fkEmpProc = Empresa.idEmpresa
+        LEFT JOIN
+            andar_de_trabalho
+        ON
+            andar_de_trabalho.fkEmpAndar = Empresa.idEmpresa
+        JOIN
+            Maquinas
+        ON
+            Maquinas.fkEmpMaq = Empresa.idEmpresa
+        LEFT JOIN
+            Login
+        ON
+            Login.Id_do_dispositivo = Maquinas.Id_do_dispositivo
+        WHERE
+            Empresa.idEmpresa = 1 AND Maquinas.fkAndarDeTrabalho = ${idAndar};`;
+    }
+    console.log("Executando listarProcessos com filtro: \n" + instrucao);
     return database.executar(instrucao);
 }
 
